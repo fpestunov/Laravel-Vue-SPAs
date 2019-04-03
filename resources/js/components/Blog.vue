490bb0376dece84b9992974dc4eb71b16fa08af3
Fedor Pestunov <fpestunov@gmail.com>
2019-04-03 09:13:14 +0400

    add new components

---
+++ resources/js/components/Blog.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1 class="font-normal text-3xl text-grey-darkest">Blog</h1>
+</template>
+
+<script>
+    export default {};
+</script>
---
+++ resources/js/components/Chatty.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1 class="font-normal text-3xl text-grey-darkest">Chatty</h1>
+</template>
+
+<script>
+    export default {};
+</script>
